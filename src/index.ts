e1c26202f1f302afdf143bada13bd1d63635b5e6
장혁수 <hyuksoo128@gmail.com>
2023-04-09 02:23:11 +0900

    Fix index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,36 @@
-export * from './isHangul';
+export { isCho, isChoByGroups } from "./isCho";
+export { isJung, isJungByGroups } from "./isJung";
+export { isJong, isJongByGroups } from "./isJong";
+export { isHangul, isHangulByGroups } from "./isHangul";
+
+export {
+  divideByJong,
+  divideByJung,
+  divideHangul,
+  divideHangulByGroups
+} from "./divide";
+
+export {
+  combineHangul,
+  combineByJung,
+  combineByJong,
+} from "./combine";
+
+export * from "./formatDate";
+export * from "./formatNumber";
+
+export * from "./josa";
+export * from "./banmal";
+
+export * from "./encode";
+export * from './distance';
+
+export * from "./includesByCho";
+export * from "./sortHangul";
+
+export * from "./getLocal";
+export * from "./convertKey";
+
+export * from './normalize';
+
+export * from './utils';
