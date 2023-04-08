944d40d6fee95e01973e5ed94e76fde27b9ccd13
장혁수 <hyuksoo128@gmail.com>
2023-04-09 02:20:27 +0900

    Add types.ts

---
+++ src/types.ts
@@ -0,0 +1,11 @@
+export type LocalTypes = "ko" | "en" | "number" | "special" | "etc";
+
+export type DivideOptionTypes = {
+  isSplit?: boolean;
+  resultType?: "object" | "string" | "array" | "index";
+};
+
+export type TypingOptionTypes = {
+  content?: string;
+  speed?: number;
+};
