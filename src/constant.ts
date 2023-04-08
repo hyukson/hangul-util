ac7bb78a71da05cf56516fd0b250af7e84d8c257
장혁수 <hyuksoo128@gmail.com>
2023-04-09 02:11:49 +0900

    Fix constant.ts

--- src/constant.ts
+++ src/constant.ts
@@ -17,50 +17,105 @@ export const JONG_HANGUL = [
   'ㅇ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ','ㅎ',
 ];
 
-export const CHO_START_CHARCODE = CHO_HANGUL[0].charCodeAt(0);
-export const CHO_END_CHARCODE = CHO_HANGUL[CHO_HANGUL.length-1].charCodeAt(0);
+export const JUNG_COMPLETE_HANGUL: Record<string, string> = {
+  ㅘ: 'ㅗㅏ',
+  ㅙ: 'ㅗㅐ',
+  ㅚ: 'ㅗㅣ',
+  ㅝ: 'ㅜㅓ',
+  ㅞ: 'ㅜㅔ',
+  ㅟ: 'ㅜㅣ',
+  ㅢ: 'ㅡㅣ',
+};
+
+export const JONG_COMPLETE_HANGUL: Record<string, string> = {
+  ㄳ: 'ㄱㅅ',
+  ㄵ: 'ㄴㅈ',
+  ㄶ: 'ㄴㅎ',
+  ㄺ: 'ㄹㄱ',
+  ㄻ: 'ㄹㅁ',
+  ㄼ: 'ㄹㅂ',
+  ㄽ: 'ㄹㅅ',
+  ㄾ: 'ㄹㅌ',
+  ㄿ: 'ㄹㅍ',
+  ㅀ: 'ㄹㅎ',
+  ㅄ: 'ㅂㅅ',
+};
 
 export const JUNG_START_CHARCODE = JUNG_HANGUL[0].charCodeAt(0);
 export const JUNG_END_CHARCODE = JUNG_HANGUL[JUNG_HANGUL.length-1].charCodeAt(0);
 
+export const JONG_START_CHARCODE = JONG_HANGUL[1].charCodeAt(0);
+export const JONG_END_CHARCODE = JONG_HANGUL[JONG_HANGUL.length-1].charCodeAt(0);
+
 export const HANGUL_START_CHARCODE = '가'.charCodeAt(0);
 export const HANGUL_END_CHARCODE = '힣'.charCodeAt(0);
 
-export const CHO_PERIOD = Math.floor('까'.charCodeAt(0) - '가'.charCodeAt(0));
-export const JUNG_PERIOD = Math.floor('개'.charCodeAt(0) - '가'.charCodeAt(0));
+export const CHO_PERIOD = '까'.charCodeAt(0) - '가'.charCodeAt(0);
+export const JONG_PERIOD = '개'.charCodeAt(0) - '가'.charCodeAt(0);
 
-export const KEY_MAPS = {
-  'ㄱ': 'r',
-  'ㄲ': 'R',
-  'ㄴ': 's',
-  'ㄷ': 'e',
-  'ㄸ': 'E',
-  'ㄹ': 'f',
-  'ㅁ': 'a',
-  'ㅂ': 'q',
-  'ㅃ': 'Q',
-  'ㅅ': 't',
-  'ㅆ': 'T',
-  'ㅇ': 'd',
-  'ㅈ': 'w',
-  'ㅉ': 'W',
-  'ㅊ': 'c',
-  'ㅋ': 'z',
-  'ㅌ': 'x',
-  'ㅍ': 'v',
-  'ㅎ': 'g',
-  'ㅏ': 'k',
-  'ㅐ': 'o',
-  'ㅑ': 'i',
-  'ㅒ': 'O',
-  'ㅓ': 'j',
-  'ㅔ': 'p',
-  'ㅕ': 'u',
-  'ㅖ': 'P',
-  'ㅗ': 'h',
-  'ㅛ': 'y',
-  'ㅜ': 'n',
-  'ㅠ': 'b',
-  'ㅡ': 'm',
-  'ㅣ': 'l',
+// use covertKey function
+export const KEY_MAPS: Record<string, string> = {
+  ㅂ: "q", ㅃ: "Q", ㅈ: "w", ㅉ: "W",
+  ㄷ: "e", ㄸ: "E", ㄱ: "r", ㄲ: "R",
+  ㅅ: "t", ㅆ: "T", ㅛ: "y", ㅕ: "u",
+  ㅑ: "i", ㅐ: "o", ㅒ: "O", ㅔ: "p", 
+  ㅖ: "P", ㅁ: "a", ㄴ: "s",ㅇ: "d",
+  ㄹ: "f", ㅎ: "g", ㅗ: "h",ㅓ: "j",
+  ㅏ: "k", ㅣ: "l", ㅋ: "z", ㅌ: "x",
+  ㅊ: "c", ㅍ: "v", ㅠ: "b", ㅜ: "n",
+  ㅡ: "m",
 };
+
+// use formatNumber function
+export const numberUnits = ["", "일", "이", "삼", "사", "오", "육", "칠", "팔", "구"];
+
+export const tenUnits = ["", "십", "백", "천"];
+
+export const thousandUnits = ["", "만", "억", "조", "경", "해"];
+
+// use formatDate function
+export const WEEK_DAY = ["일", "월", "화", "수", "목", "금", "토"];
+
+// use josa function
+export const JOSA_LIST: Record<string, string> = {
+  이: "이/가",
+  가: "이/가",
+  을: "을/를",
+  를: "을/를",
+  은: "은/는",
+  는: "은/는",
+  으로: "으로/로",
+  로: "으로/로",
+  와: "와/과",
+  과: "와/과",
+  이나: "이나/나",
+  나: "이나/나",
+  이에: "이에/에",
+  에: "이에/에",
+  이란: "이란/란",
+  란: "이란/란",
+}
+
+// use normalize function
+export const NORMALIZE_CHO = [
+  "g", "gg", "n", "d", "dd",
+  "r", "m", "b", "bb", "s",
+  "ss", "", "j", "jj", "c",
+  "k", "t", "p", "h"
+];
+
+export const NORMALIZE_JUNG = [
+ "a", "ae", "ya", "yae", "eo",
+ "e", "yeo", "ye", "o", "wa",
+ "wae", "oe", "yo", "u", "weo",
+ "we", "wi", "yu", "eu", "eui",
+ "i"
+]
+export const NORMALIZE_JONG = [
+  "", "g", "gg", "gs", "n", "nj",
+  "nh", "d", "r", "rk", "rm", 
+  "rb", "rs", "rt", "rp", "rh", 
+  "m", "b", "bs", "s", "ss", 
+  "ng", "j", "c", "k", "t", 
+  "p", "h"
+]
