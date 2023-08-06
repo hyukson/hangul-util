c21cf93c9f5cd481a275ef8877178be8bc48fc24
장혁수 <hyuksoo128@gmail.com>
2023-08-06 22:32:16 +0900

    fix: window 객체명 변경

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "./dist"),
     filename: "bundle.js",
-    library: "Hangul",
+    library: "H",
     libraryTarget: "window",
   },
   module: {
